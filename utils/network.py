4b4b7da93372e1ed3a3b7ee05e7f0b34970d774d
Kevin Booth <kevin@kb.gg>
2019-06-21 09:41:00 -0500

    Fixed ssl verification on downloading files

--- utils/network.py
+++ utils/network.py
@@ -4,24 +4,23 @@ import shutil
 import json
 import os
 
-def request(url, data=None):
+def ssl_context():
     if os.environ.get('STAC_DEBUG', False):
-        context = ssl._create_unverified_context()
-    else:
-        context = ssl.SSLContext()
+        return ssl._create_unverified_context()
+    return ssl.SSLContext()
 
+def request(url, data=None):
     r = urllib.request.Request(url)
     if data is not None:
         body_bytes = json.dumps(data).encode('utf-8')
         r.add_header('Content-Type', 'application/json; charset=utf-8')
         r.add_header('Content-Length', len(body_bytes))
-        r = urllib.request.urlopen(r, body_bytes, context=context, timeout=5)
+        r = urllib.request.urlopen(r, body_bytes, context=ssl_context(), timeout=5)
     else:
-        r = urllib.request.urlopen(r, context=context, timeout=5)
+        r = urllib.request.urlopen(r, context=ssl_context(), timeout=5)
 
     return json.loads(r.read())
 
 def download(url, path):
-    context = ssl._create_unverified_context()
-    with urllib.request.urlopen(url, context=context, timeout=5) as response, open(path, 'wb') as f:
+    with urllib.request.urlopen(url, context=ssl_context(), timeout=5) as response, open(path, 'wb') as f:
         shutil.copyfileobj(response, f)
